77d33da19a9f3e6bc0a26162c1c97d49a4220429
DevPageTest <testfordevwork@gmail.com>
2024-06-02 02:13:42 -0400

    Add files via upload

--- assets/dropdown.js
+++ assets/dropdown.js
@@ -1,23 +1,25 @@
-//Dropdown
-const dropdownContainer = document.querySelector(".dropdown-content");
-let dropdownHeight = 0;
-const btn = document.querySelector(".dropbtn");
-const dropdownArray = document.getElementsByClassName("dropdown-content-a");
+document.addEventListener("DOMContentLoaded", function () {
+  //Dropdown
+  const dropdownContainer = document.querySelector(".dropdown-content");
+  let dropdownHeight = 0;
+  const btn = document.querySelector(".dropbtn");
+  const dropdownArray = document.getElementsByClassName("dropdown-content-a");
 
-for (let i = 0; i < dropdownArray.length; i++) {
-  let itemHeight = +dropdownArray[i].offsetHeight;
-  dropdownHeight += itemHeight;
-}
+  for (let i = 0; i < dropdownArray.length; i++) {
+    let itemHeight = +dropdownArray[i].offsetHeight;
+    dropdownHeight += itemHeight;
+  }
 
-function handleHover() {
-  dropdownContainer.style.height = `${dropdownHeight}px`;
-}
+  function handleHover() {
+    dropdownContainer.style.height = `${dropdownHeight}px`;
+  }
 
-function handleLeaveHover() {
-  dropdownContainer.style.height = `0px`;
-}
+  function handleLeaveHover() {
+    dropdownContainer.style.height = `0px`;
+  }
 
-btn.addEventListener("mouseenter", handleHover);
-dropdownContainer.addEventListener("mouseenter", handleHover);
-btn.addEventListener("mouseleave", handleLeaveHover);
-dropdownContainer.addEventListener("mouseleave", handleLeaveHover);
+  btn.addEventListener("mouseenter", handleHover);
+  dropdownContainer.addEventListener("mouseenter", handleHover);
+  btn.addEventListener("mouseleave", handleLeaveHover);
+  dropdownContainer.addEventListener("mouseleave", handleLeaveHover);
+});
---
+++ info/info.js
@@ -0,0 +1,374 @@
+const info = {
+  title: "Rol Chile",
+  homeBtn: {
+    href: "https://devpagetest.github.io/TestForDev/",
+    src: "./assets/images/logov2h.png",
+    alt: "Logo",
+  },
+  navLinks: [
+    {
+      title: "Opción 1",
+      url: "https://devpagetest.github.io/TestForDev/",
+    },
+    { title: "Opción 2", url: "https://devpagetest.github.io/TestForDev/" },
+  ],
+  dropdownBtn: {
+    title: "Opción 3",
+    dropdownLinks: [
+      { title: "Actividades", href: "#activities" },
+      { title: "Información", href: "#information" },
+      { title: "Plataformas", href: "#platforms" },
+    ],
+  },
+  socialsBtns: [
+    {
+      href: "https://devpagetest.github.io/TestForDev/",
+      alt: "discord logo",
+      src: "./assets/images/logo-discord.png",
+    },
+    {
+      href: "https://devpagetest.github.io/TestForDev/",
+      alt: "faceboook logo",
+      src: "./assets/images/logo-facebook.png",
+    },
+    {
+      href: "https://devpagetest.github.io/TestForDev/",
+      alt: "tiktok logo",
+      src: "./assets/images/logo-tiktok.png",
+    },
+  ],
+  headerTitle: { text: "La Aventura te espera!" },
+  headerSecondText: { text: "Forma parte de la comunidad más grande de rol." },
+  backgroundImage: { src: "./assets/images/background.jpg", alt: "Portada" },
+  backgroundSideImage: {
+    src: "./assets/images/party.png",
+    alt: "Image sobrepuesta",
+  },
+  activitiesTitle: { text: "Nuestras actividades" },
+  activitiesList: [
+    {
+      src: "./assets/images/lista1.jpg",
+      alt: "Imagen actividad 1",
+      title: "Super actividad",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam distinctio sed corrupti, nobis tenetur harum unde asperiores est animi iure.",
+    },
+    {
+      src: "./assets/images/lista2.jpg",
+      alt: "Imagen actividad 2",
+      title: "Super actividad",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam distinctio sed corrupti, nobis tenetur harum unde asperiores est animi iure.",
+    },
+    {
+      src: "./assets/images/lista3.jpg",
+      alt: "Imagen actividad 3",
+      title: "Super actividad",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam distinctio sed corrupti, nobis tenetur harum unde asperiores est animi iure.",
+    },
+    {
+      imgsrc: "./assets/images/both.png",
+      imgalt: "Imagen hombre lobo y vampiro",
+    },
+    {
+      src: "./assets/images/lista4.jpg",
+      alt: "Imagen actividad 4",
+      title: "Super actividad",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam distinctio sed corrupti, nobis tenetur harum unde asperiores est animi iure.",
+    },
+    {
+      src: "./assets/images/lista5.jpg",
+      alt: "Imagen actividad 5",
+      title: "Super actividad",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam distinctio sed corrupti, nobis tenetur harum unde asperiores est animi iure.",
+    },
+  ],
+  activitiesContainerInfo: {
+    src: "./assets/images/dice.png",
+    alt: "Dados",
+    text: "Juegos de rol, eventos, comunidad, escuelas de rol, discord y mucho más!",
+  },
+  activitiesSocials: {
+    discordSocials: {
+      title: "Conoce nuestro nuevo discord",
+      href: "https://discord.gg",
+      secondTitle: "Nuestras Redes Sociales",
+      socials: [
+        {
+          href: "www.tiktok.com",
+          src: "./assets/images/tik.png",
+          alt: "Tiktok",
+        },
+        {
+          href: "www.instagram.com",
+          src: "./assets/images/ig.png",
+          alt: "Instagram",
+        },
+        {
+          href: "www.facebook.com",
+          src: "./assets/images/fb.png",
+          alt: "Facebook",
+        },
+      ],
+    },
+  },
+  platforms: {
+    title: "Nuestras Plataformas",
+    list: [
+      {
+        href: "#",
+        src: "./assets/images/WolfGate.png",
+        alt: "Wolfgate",
+        title: "Wolfgate",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem neque doloremque qui asperiores facilis quas reprehenderit nesciunt earum culpa explicabo.",
+        btnText: "Ir a la página",
+      },
+      {
+        href: "#",
+        src: "./assets/images/Eventos.png",
+        alt: "Eventos",
+        title: "Planificador de Eventos",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem neque doloremque qui asperiores facilis quas reprehenderit nesciunt earum culpa explicabo.",
+        btnText: "Ir a la página",
+      },
+      {
+        href: "#",
+        src: "./assets/images/BuscaRol.png",
+        alt: "Buscador",
+        title: "Buscador de Rol",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem neque doloremque qui asperiores facilis quas reprehenderit nesciunt earum culpa explicabo.",
+        btnText: "Ir a la página",
+      },
+      {
+        href: "#",
+        src: "./assets/images/JuegaRol.png",
+        alt: "Juega Rol",
+        title: "Juega Rol",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem neque doloremque qui asperiores facilis quas reprehenderit nesciunt earum culpa explicabo.",
+        btnText: "Ir a la página",
+      },
+    ],
+  },
+  communities: {
+    img: { src: "./assets/images/enano.png", alt: "Enano" },
+    carousel: [
+      {
+        img: { src: "./assets/images/lista1.jpg", alt: "Imagen comunidad" },
+        title: "Comunidad del Dragon Rojo",
+        text: "Comunidad de puros jugadores con alineamiento Maligno.",
+        button: {
+          href: "https://devpagetest.github.io/TestForDev/",
+          text: "Ir",
+        },
+      },
+      {
+        img: { src: "./assets/images/lista1.jpg", alt: "Imagen comunidad" },
+        title: "Comunidad de la Paradoja",
+        text: "Comunidad sobre el juego de rol Mago.",
+        button: {
+          href: "https://devpagetest.github.io/TestForDev/",
+          text: "Ir",
+        },
+      },
+      {
+        img: { src: "./assets/images/lista1.jpg", alt: "Imagen comunidad" },
+        title: "Comunidad del Anillo",
+        text: "Comunidad sobre el Señor de los Anillos.",
+        button: {
+          href: "https://devpagetest.github.io/TestForDev/",
+          text: "Ir",
+        },
+      },
+      {
+        img: { src: "./assets/images/lista1.jpg", alt: "Imagen comunidad" },
+        title: "Comunidad de la espada roja",
+        text: "Comunidad especializada en Dungeons and Dragons.",
+        button: {
+          href: "https://devpagetest.github.io/TestForDev/",
+          text: "Ir",
+        },
+      },
+      {
+        img: { src: "./assets/images/lista1.jpg", alt: "Imagen comunidad" },
+        title: "Pony Pisador",
+        text: "Grupo LARP sobre el Señor de los Anillos.",
+        button: {
+          href: "https://devpagetest.github.io/TestForDev/",
+          text: "Ir",
+        },
+      },
+      {
+        img: { src: "./assets/images/lista1.jpg", alt: "Imagen comunidad" },
+        title: "Comunidad de los 7 Colores",
+        text: "Grupo que habla sobre los magos del Señor de los Anillos.",
+        button: {
+          href: "https://devpagetest.github.io/TestForDev/",
+          text: "Ir",
+        },
+      },
+      {
+        img: { src: "./assets/images/lista1.jpg", alt: "Imagen comunidad" },
+        title: "Guardianes del Amanecer",
+        text: "Grupo que habla sobre Werewolf the Apocalypse.",
+        button: {
+          href: "https://devpagetest.github.io/TestForDev/",
+          text: "Ir",
+        },
+      },
+    ],
+  },
+};
+
+function populatePage() {
+  document.title = info.title;
+  document.querySelector(".header_navigation-home").href = info.homeBtn.href;
+  document.querySelector(".header_navigation-home--logo").src =
+    info.homeBtn.src;
+  document.querySelector(".header_navigation-home--logo").alt = "Logo";
+  //Top links and dropdown
+  const navLinks = document.querySelector(".header_navigation-options-anchors");
+  info.navLinks.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `<a href="${item.title}">${item.title}</a>`;
+    navLinks.appendChild(listItem);
+  });
+
+  document.querySelector(".dropbtn").textContent = info.dropdownBtn.title;
+
+  const dropLinks = document.querySelector(".dropdown-content");
+  info.dropdownBtn.dropdownLinks.forEach((item) => {
+    const listItem = document.createElement("a");
+    listItem.classList.add("dropdown-content-a");
+    listItem.setAttribute("href", `${item.href}`);
+    listItem.innerHTML = `${item.title}`;
+    dropLinks.appendChild(listItem);
+  });
+
+  const socialLinks = document.querySelector(
+    ".header_navigation-options-socials"
+  );
+  info.socialsBtns.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `<a class="socialBtn" href="${item.href}"><img alt="${item.alt}" src="${item.src}"/></a>`;
+    socialLinks.appendChild(listItem);
+  });
+
+  document.querySelector(".header_text-title").textContent =
+    info.headerTitle.text;
+  document.querySelector(".header_text-second").textContent =
+    info.headerSecondText.text;
+  document.querySelector(".header_background").src = info.backgroundImage.src;
+  document.querySelector(".header_background").alt = info.backgroundImage.alt;
+  document.querySelector(".header_image").src = info.backgroundSideImage.src;
+  document.querySelector(".header_image").alt = info.backgroundSideImage.alt;
+  document.querySelector(".activities_container-title").textContent =
+    info.activitiesTitle.text;
+
+  const activities = document.querySelector(".activities_container-list");
+  info.activitiesList.forEach((item, index) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("activities_container-list--item");
+    if (index === 3) {
+      listItem.classList.add("image");
+      listItem.innerHTML = `            
+      <img
+        class="activities_image"
+        src="${item.imgsrc}"
+        alt="${item.imgalt}"
+      />`;
+    } else {
+      listItem.innerHTML = `            
+    <div class="list_image_container">
+        <img
+          class="list_image"
+          src="${item.src}"
+          alt="${item.alt}"
+        />
+    </div>
+    <div class="list_text_container">
+      <h2 class="list_title">${item.title}</h2>
+      <p class="list_text">${item.text}</p>
+    </div>`;
+    }
+    activities.appendChild(listItem);
+  });
+
+  document.querySelector(".activities_container-info").innerHTML = `
+    <img
+      class="activities_container-info--img"
+      src="${info.activitiesContainerInfo.src}"
+      alt="${info.activitiesContainerInfo.alt}"
+    />
+    <div class="activities_container-info--text">
+      <p>${info.activitiesContainerInfo.text}</p>
+    </div>
+  `;
+  document.querySelector(".information_discord").innerHTML = `
+    <img src="assets/images/discord-white.png" alt="Discord logo" />
+    <h1>${info.activitiesSocials.discordSocials.title}</h1>
+    <a href="${info.activitiesSocials.discordSocials.href}">https://discord.gg</a>
+  `;
+  document.querySelector(".social_title").textContent =
+    info.activitiesSocials.discordSocials.secondTitle;
+
+  const infoLinks = document.querySelector(".socials_imgs");
+  info.activitiesSocials.discordSocials.socials.forEach((item) => {
+    const listItem = document.createElement("a");
+    listItem.setAttribute("href", `${item.href}`);
+    listItem.innerHTML = `<img src="${item.src}" alt="${item.alt}" />`;
+    infoLinks.appendChild(listItem);
+  });
+
+  document.querySelector(".plataforms_title").textContent =
+    info.platforms.title;
+
+  const platforms = document.querySelector(".platforms_ul");
+  info.platforms.list.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("platforms_li");
+    listItem.innerHTML = `
+    <a href="${item.href}" style="background-image: url(assets/images/back.jpg)">
+        <div class="platforms_li-container">
+          <img src="${item.src}" alt="${item.alt}" />
+          <h2>${item.title}</h2>
+        </div>
+        <div class="platforms_li-text">
+          <div class="platforms_li-text--txtbtn">
+            <p>${item.text}</p>
+            <button>${item.btnText}</button>
+          </div>
+        </div>
+    </a>
+    `;
+    platforms.appendChild(listItem);
+  });
+
+  document
+    .querySelector(".side_img")
+    .setAttribute("src", `${info.communities.img.src}`);
+  document
+    .querySelector(".side_img")
+    .setAttribute("alt", `${info.communities.img.alt}`);
+
+  const carousel = document.querySelector(".carousel");
+  info.communities.carousel.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("slide");
+    console.log(item.img);
+    listItem.innerHTML = `
+    <div class="slide-img">
+    <img
+      src="${item.img.src}"
+      alt="${item.img.alt}"
+      draggable="false"
+    />
+    </div>
+    <div class="slide-paragraph">
+      <h2>${item.title}</h2>
+      <p>${item.text}</p>
+      <a href="${item.button.href}">${item.button.text}</a>
+    </div>
+      `;
+    carousel.appendChild(listItem);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", populatePage);
